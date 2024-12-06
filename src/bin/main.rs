f2899adf97f9e64e8eccc64df3b4776180f09a36
nulltrek <nulltrek@users.noreply.github.com>
2024-12-06 22:49:45 +0100

    Testing transaction building using local bitcoin node

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,57 @@
+use bitlib::block::BlockId;
+use bitlib::builders::TxBuilder;
 use bitlib::definitions::{Compression, Network};
 use bitlib::ecdsa::{PrivateKey, Secp256k1};
 use bitlib::hashing::hash256;
+use bitlib::network::{NetworkError, NodeClient};
+use bitlib::tx::{TxFetcher, TxId};
 use bitlib::u256::U256;
+use std::env;
+use ureq::Error;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please specify cookie path");
+        return;
+    }
+
     println!("Generating testnet address");
     let secp = Secp256k1::new();
     let secret_hash = hash256("Wherever Two Or More Are Gathered... As The Earth Kissed The Moon");
-    let pubkey = secp.get_pubkey(&PrivateKey::new(U256::from_little_endian(&secret_hash)));
-    // mvjLH9YikB9kuWbw8C6uE1SSY54QqvNzQX
-    // n3ueoiZVnRaGqxDCdZtQcKYeMa4oznzmiY
+    let privkey = PrivateKey::new(U256::from_little_endian(&secret_hash));
+    let pubkey = secp.get_pubkey(&privkey);
     println!("{}", pubkey.to_address(Compression::Yes, Network::Test));
+    // migshLXU9p2VFDzeV3AwKC6EHQuVXuUGeQ
+
+    let mut client =
+        NodeClient::new("http://127.0.0.1:18443", std::path::Path::new(&args[1])).unwrap();
+    let block = client
+        .fetch_block_header(&BlockId::from(U256::from_hex(
+            "2b21398988ebe31b4bec42f385763802c555537d23a5a1f35fc00f73f9bc4c0e",
+        )))
+        .unwrap();
+    println!("Block: {}\n", block);
+    let prev_tx = client
+        .fetch_tx(&TxId::from(U256::from_hex(
+            "d31df99a4c65b9ef257a84217bb7b7588b573789cb6a21ecd274e5e8100c5844",
+        )))
+        .unwrap();
+    println!("{}", prev_tx.id());
+
+    let tx = TxBuilder::new()
+        .add_input(prev_tx.hash(), 0, 0)
+        .add_output(2500000000, &pubkey)
+        .add_output(2499999500, &pubkey)
+        .build(&mut client, &privkey)
+        .unwrap();
+    println!("{}", tx);
+
+    let result = client.send_tx(&tx);
+    match result {
+        Err(NetworkError::RequestError(Error::Status(code, response))) => {
+            println!("{} - {:?}", code, response.into_string())
+        }
+        other => println!("{:?}", other),
+    };
 }
--- src/network.rs
+++ src/network.rs
@@ -1,13 +1,21 @@
+use crate::block::{BlockError, BlockHeader, Result as BlockResult};
 use crate::definitions::Network;
-use crate::hashing::from_hex_str;
-use crate::tx::{Result as TxResult, Tx, TxError, TxFetcher, TxId};
+use crate::hashing::{from_hex_str, to_hex_str, Hash};
+use crate::tx::{Result as TxResult, Tx, TxError, TxFetcher, TxId, TxSerType};
+use crate::u256::U256;
+use base64::prelude::*;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 use ureq;
 
 #[derive(Debug)]
 pub enum NetworkError {
     RequestError(ureq::Error),
     IoError(std::io::Error),
+    ParsingError(serde_json::Error),
     InvalidData,
 }
 
@@ -23,20 +31,13 @@ impl From<std::io::Error> for NetworkError {
     }
 }
 
-pub type Result<T> = std::result::Result<T, NetworkError>;
-
-fn get_host(network: Network) -> &'static str {
-    match network {
-        Network::Main => "https://blockstream.info/api",
-        Network::Test => "https://blockstream.info/testnet/api",
+impl From<serde_json::Error> for NetworkError {
+    fn from(error: serde_json::Error) -> Self {
+        NetworkError::ParsingError(error)
     }
 }
 
-fn fetch(network: Network, path: &str) -> Result<String> {
-    Ok(ureq::get(&format!("{}{}", get_host(network), path))
-        .call()?
-        .into_string()?)
-}
+pub type Result<T> = std::result::Result<T, NetworkError>;
 
 pub struct NetFetcher {
     network: Network,
@@ -50,6 +51,19 @@ impl NetFetcher {
             cache: HashMap::new(),
         }
     }
+
+    fn get_host(network: Network) -> &'static str {
+        match network {
+            Network::Main => "https://blockstream.info/api",
+            Network::Test => "https://blockstream.info/testnet/api",
+        }
+    }
+
+    fn fetch(network: Network, path: &str) -> Result<String> {
+        Ok(ureq::get(&format!("{}{}", Self::get_host(network), path))
+            .call()?
+            .into_string()?)
+    }
 }
 
 impl TxFetcher for NetFetcher {
@@ -60,7 +74,7 @@ impl TxFetcher for NetFetcher {
             return Ok(tx.clone());
         }
 
-        let body = fetch(self.network, &format!("/tx/{}/hex", id))?;
+        let body = Self::fetch(self.network, &format!("/tx/{}/hex", id))?;
         match from_hex_str(&body) {
             Err(_) => Err(TxError::NetworkError(NetworkError::InvalidData)),
             Ok(bytes) => {
@@ -72,6 +86,150 @@ impl TxFetcher for NetFetcher {
                 Ok(tx)
             }
         }
+    }
+}
+
+pub struct NodeClient {
+    url: String,
+    cookie: String,
+    txs: HashMap<String, Tx>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RPCResult<T> {
+    result: T,
+    error: Option<String>,
+    id: String,
+}
+
+impl NodeClient {
+    pub fn new(url: &str, cookie: &Path) -> Result<NodeClient> {
+        let mut client = Self {
+            url: url.to_string(),
+            cookie: fs::read_to_string(cookie)?,
+            txs: HashMap::new(),
+        };
+
+        #[derive(Debug, Deserialize)]
+        struct ChainTip {
+            height: u32,
+            hash: String,
+            branchlen: u32,
+            status: String,
+        }
+        let body = client.call("getchaintips", json!([]))?;
+        let data = match serde_json::from_str::<RPCResult<Vec<ChainTip>>>(&body) {
+            Err(_) => return Ok(client),
+            Ok(data) => data,
+        };
+
+        #[derive(Debug, Deserialize)]
+        struct Block {
+            previousblockhash: Option<String>,
+            tx: Vec<String>,
+        }
+        let mut block_hash = data.result[0].hash.clone();
+        loop {
+            let body = client.call("getblock", json!([&block_hash]))?;
+            let block = match serde_json::from_str::<RPCResult<Block>>(&body) {
+                Err(err) => panic!("{}", err),
+                Ok(data) => data.result,
+            };
+            if block.previousblockhash.is_none() {
+                break;
+            }
+            for tx_hash in block.tx {
+                let tx = client
+                    .fetch_tx_from_block(
+                        &Hash::from(U256::from_hex(&tx_hash)),
+                        &Hash::from(U256::from_hex(&block_hash)),
+                    )
+                    .unwrap();
+                client.txs.insert(tx_hash, tx);
+            }
+            block_hash = block.previousblockhash.unwrap();
+        }
+
+        Ok(client)
+    }
+
+    pub fn call(&self, method: &str, params: Value) -> Result<String> {
+        let resp = ureq::post(&self.url)
+            .set("Content-Type", "text/plain")
+            .set(
+                "Authorization",
+                &std::format!("Basic {}", BASE64_STANDARD.encode(self.cookie.as_bytes())),
+            )
+            .send_string(
+                &json!({
+                     "jsonrpc": "1.0",
+                     "id": "bitlib",
+                     "method": method,
+                     "params": params
+                })
+                .to_string(),
+            )?
+            .into_string()?;
+        Ok(resp)
+    }
+
+    pub fn fetch_block_header(&self, id: &Hash) -> BlockResult<BlockHeader> {
+        let body = self.call("getblockheader", json!([&id.to_string(), false]))?;
+        let data: RPCResult<String> = match serde_json::from_str(&body) {
+            Err(err) => return Err(BlockError::NetworkError(NetworkError::ParsingError(err))),
+            Ok(data) => data,
+        };
+        match from_hex_str(&data.result) {
+            Err(_) => Err(BlockError::NetworkError(NetworkError::InvalidData)),
+            Ok(bytes) => {
+                let tx = match BlockHeader::parse(&bytes) {
+                    Err(err) => return Err(err),
+                    Ok(tx) => tx,
+                };
+                Ok(tx)
+            }
+        }
+    }
+
+    pub fn fetch_tx_from_block(&self, id: &TxId, block_id: &Hash) -> TxResult<Tx> {
+        log::info!("Fetching tx from node: {}", id);
+        let body = self.call(
+            "getrawtransaction",
+            json!([&id.to_string(), false, &block_id.to_string()]),
+        )?;
+        let data: RPCResult<String> = match serde_json::from_str(&body) {
+            Err(err) => return Err(TxError::NetworkError(NetworkError::ParsingError(err))),
+            Ok(data) => data,
+        };
+        match from_hex_str(&data.result) {
+            Err(_) => Err(TxError::NetworkError(NetworkError::InvalidData)),
+            Ok(bytes) => {
+                let tx = match Tx::parse(&bytes) {
+                    Err(err) => return Err(err),
+                    Ok(tx) => tx,
+                };
+                Ok(tx)
+            }
+        }
+    }
+
+    pub fn send_tx(&self, tx: &Tx) -> Result<String> {
+        println!("{}", to_hex_str(tx.serialize(TxSerType::Full)));
+        log::info!("Sending tx to node: {}", tx.id());
+        let body = self.call(
+            "sendrawtransaction",
+            json!([to_hex_str(tx.serialize(TxSerType::Full))]),
+        )?;
+        match serde_json::from_str::<RPCResult<String>>(&body) {
+            Err(err) => Err(NetworkError::ParsingError(err)),
+            Ok(data) => Ok(data.result),
+        }
+    }
+}
+
+impl TxFetcher for NodeClient {
+    fn fetch_tx(&mut self, id: &TxId) -> TxResult<Tx> {
+        Ok(self.txs.get(&id.to_string()).unwrap().clone())
     }
 }
 
